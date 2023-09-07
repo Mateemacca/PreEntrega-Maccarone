9b23a545be93d1d675192382fd3acd95779334d6
Mate Maccarone <mateobauti.maccarone@hotmail.com>
2023-09-07 15:01:20 -0300

    PreEntrega 1

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './ItemList.css'
 export default function ItemListContainer(props) {
   return (
-    <div className="bg-[#111111] mt-4  flex justify-center shadow-md">
-      <p className="text-xl text-white font-semibold">{props.greeting}</p>
+    <div className="bg-[#111111] mt-4  flex justify-center">
+      <span className="text-white gradient-text">{props.greeting}</span>
     </div>
   );
 }
